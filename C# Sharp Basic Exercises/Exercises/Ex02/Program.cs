2bade4f944d3eb09f5dc79e1ac36978734e8e1ba
FilipKufalov <filip.kufalov@hotmail.com>
2022-09-21 17:19:15 +0200

    exercises

---
+++ C# Sharp Basic Exercises/Exercises/Ex02/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter first number:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The sum of the {number1} and {number2} = {number1 + number2}");
